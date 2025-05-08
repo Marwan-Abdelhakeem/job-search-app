c9f6f7b7956831a22cf6824fc6f6ae175afa26b2
Marwan <marwan.abdelhakeem.ahmed@gmail.com>
2025-05-08 06:14:17 +0300

    v1

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
+
+const server = express();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
-  const port: number = Number(process.env.PORT);
-
-  await app.listen(port ?? 3002, () => {
-    console.log(`server is running on port ${port}`);
-  });
+  await app.init();
 }
+
 bootstrap();
+
+module.exports = server;
